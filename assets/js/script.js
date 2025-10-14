f1898e9566dd79569833ddf9564e56c57cb2917e
Thiagomuniz08 <senaithiago13@gmail.com>
2025-10-14 16:14:07 -0300

    atts

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,11 @@ async function loginUsuario(email, senha) {
 }
 
 // ------- Produtos -------
-async function listarProdutos() {
+async function listarProdutos(limit = 10) {
   const res = await fetch(`${API_URL}/produtos`);
-  return res.json();
+  const data = await res.json();
+  return data.slice(0, limit); // Mostra só os 6 primeiros produtos
 }
-
 // ------- Pedidos -------
 async function criarPedido(dados) {
   const res = await fetch(`${API_URL}/pedidos`, {
